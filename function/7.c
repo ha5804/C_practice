0ff48899769fbbf0a8541a3af610812ce9b11ab7
ha5804 <zixpotf5803@gmail.com>
2025-11-29 15:14:20 +0900

    factorial

--- function/7.c
+++ function/7.c
@@ -1,2 +1,19 @@
 #include <stdio.h>
+//Factorial의 정의에 대해 생각하자.
+//Factorial = n! =  n * (n-1)! = n * (n-1) * (n-2)!이다.
+int Factorial(int num){
+    if (num == 0){
+        return 1;
+    }
+    else{
+        return num  * Factorial(num-1);
+    }
 
+}
+int main(){
+    int n;
+    printf("Enter the num : ");
+    scanf("%d", &n);
+    printf("%d! = %d\n", n, Factorial(n));
+    return 0;
+}
